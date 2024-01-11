0d88959ae95f3a1197d02b79715049f99c91c51e
sanjayspear <sanjayspear@gmail.com>
2024-01-11 19:12:23 +0530

    Added input interactions wdio commands

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -26,3 +26,42 @@ Then(/^URL should match (.*)$/, async function (expectedURL) {
   const currentURL = await browser.getUrl();
   expect(currentURL).to.equal(expectedURL);
 });
+
+Given(/^A web page is opened$/, async function () {
+  await browser.url("/inputs");
+  await browser.setTimeout({ implicit: 15000, pageLoad: 1000 });
+  await browser.maximizeWindow();
+});
+
+When(/^Perform web interactions$/, async function () {
+  /**
+   * 1. Input box
+   * Actions:
+   * 1. Type into input box
+   * 2. Clear the field and type or just addValue
+   * 3. Click and type
+   * 4. type slowly like real user
+   *
+   */
+  let num = 9867;
+  let strNum = num.toString();
+
+  let ele = await $(`[type=number]`);
+
+  for (let i = 0; i <= strNum.length; i++) {
+    let charStr = strNum.charAt(i);
+    let _number = Number(charStr);
+    await browser.pause(1000);
+    await ele.addValue(_number);
+  }
+
+  //Approach 2
+  // await browser.pause(1000);
+
+  // await ele.click();
+  // for (let i = 0; i <= strNum.length; i++) {
+  //   let charStr = strNum.charAt(i);
+  //   await browser.pause(1000);
+  //   await browser.keys(charStr);
+  // }
+});
